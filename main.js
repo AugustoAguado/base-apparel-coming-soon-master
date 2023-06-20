2957ed89c2ccdef660fe06e7aed0405b7f6f4123
AugustoAguado <augustoaguado01@gmail.com>
2023-06-20 16:22:43 -0300

    changes

---
+++ main.js
@@ -0,0 +1,28 @@
+
+let btnSubmit = document.querySelector(".main__button");
+let successCard = document.querySelector(".main__success");
+let container = document.querySelector(".main__container");
+let successButton = document.querySelector(".main__success__button");
+let form = document.querySelector(".main__form");
+let input = document.querySelector(".main__input");
+
+
+ form.addEventListener('submit',(event)=>{
+    event.preventDefault();
+    let mail = input.value;
+    let expReg= /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+    if(mail.match(expReg)){
+        successCard.style.display = 'flex';
+        container.style.opacity ="50%"; 
+        let joined = document.querySelector(".main__success__mail");
+        joined.innerHTML = mail;
+        successButton.addEventListener("click",()=>{
+            successCard.style.display = 'none';
+            container.style.opacity ="100%"; 
+        })
+    form.reset();   
+    }
+    else{
+        input.style.border = "0.1rem solid #f60909";
+    }
+})
